c58355337ab3975d6f67a9c10465a62e24ab25ec
sik02 <pkwaw@naver.com>
2022-11-08 01:49:36 +0900

    searchpage 추가

---
+++ src/pages/search.jsx
@@ -0,0 +1,110 @@
+import React, { useMemo } from "react";
+
+import Template from "../components/Common/Template";
+import PostList from "../components/Main/PostList";
+import CategoryList from "../components/Main/CategoryList";
+import { graphql } from "gatsby";
+import queryString from "query-string";
+
+const SearchPage = ({
+  location: { search },
+  data: {
+    site: {
+      siteMetadata: { title, description, siteUrl },
+    },
+    allMarkdownRemark: { edges },
+    file: {
+      childImageSharp: { gatsbyImageData },
+      publicURL,
+    },
+  },
+}) => {
+  const parsed = queryString.parse(search);
+
+  const selectedCategory =
+    typeof parsed.category !== "string" || !parsed.category
+      ? "All"
+      : parsed.category;
+
+  const categoryList = useMemo(
+    () =>
+      edges.reduce(
+        (
+          list,
+          {
+            node: {
+              frontmatter: { categories },
+            },
+          }
+        ) => {
+          categories.forEach((category) => {
+            if (list[category] === undefined) list[category] = 1;
+            else list[category]++;
+          });
+
+          list["All"]++;
+
+          return list;
+        },
+        { All: 0 }
+      ),
+    []
+  );
+  return (
+    <Template
+      title={title}
+      description={description}
+      url={siteUrl}
+      image={publicURL}
+    >
+      <CategoryList
+        selectedCategory={selectedCategory}
+        categoryList={categoryList}
+      />
+      <PostList selectedCategory={selectedCategory} posts={edges} />
+    </Template>
+  );
+};
+
+export default SearchPage;
+
+export const getPostList = graphql`
+  query getPostList {
+    site {
+      siteMetadata {
+        title
+        description
+        siteUrl
+      }
+    }
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData(width: 768, height: 400)
+              }
+            }
+          }
+        }
+      }
+    }
+    file(name: { eq: "profile-image" }) {
+      childImageSharp {
+        gatsbyImageData(width: 120, height: 120)
+      }
+      publicURL
+    }
+  }
+`;
